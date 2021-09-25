1874e6932ea7992a1a65345ce851683073c62791
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-09-25 12:45:04 +1000

    Added graph loading

--- code/spark/build.sbt
+++ code/spark/build.sbt
@@ -12,7 +12,10 @@ libraryDependencies += "log4j" % "log4j" % "1.2.17"
 libraryDependencies += "dev.zio" %% "zio" % "2.0.0-M2"
 libraryDependencies += "dev.zio" %% "zio-streams" % "2.0.0-M2"
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.0.0"
+libraryDependencies += "org.typelevel" %% "cats-core" % "2.3.0"
 libraryDependencies += "org.typelevel" %% "cats-effect" % "2.5.3" withSources() withJavadoc()
+libraryDependencies += "org.typelevel" %% "cats-collections-core" % "0.9.0"
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.3.5"
 
 scalacOptions ++= Seq(
   "-feature",
--- code/spark/src/main/scala/org/isub/thesis/app/App.scala
+++ code/spark/src/main/scala/org/isub/thesis/app/App.scala
@@ -4,31 +4,41 @@ import scala.io.Source
 import scala.util.Either
 import zio.Console._
 import org.apache.spark.graphx.{Edge, EdgeRDD, Graph, VertexRDD}
+import org.apache.spark._
+import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.commons.lang3.exception.ExceptionUtils
 import zio.ZIO
 import zio.UIO
 import zio.URIO
-
+import cats._
+import scalaz._
+import Scalaz._
 
 
 object GraphParser {
-  def splitString(file: String): Either[String, (Int, Int)] = {
-    val arr = file.split(" ").map(_.toInt)
+  def splitString(file: String): Either[String, (Long, Long, Long)] = {
+    val arr = file.split(" ").map(_.toLong)
     if (arr.length != 2) {
-      val tup = (arr(0), arr(1))
+      val tup = (arr(0), arr(1), 1L)
       return Right(tup)
     }else {
       return Left("Need exactly 2 nodes per line")
     }
   }
 
-  def parse(file: String) =  zio.IO {
-    Source
-    .fromFile(file)
-    .getLines()
-    .map(splitString)
+  def parse(file: String, sc: SparkContext) =  zio.IO {
+    // Either[Error, (NodeID, NodeID, Weight)]
+
+    for {
+      edges <- Source.fromFile(file).getLines().map(splitString).toList.sequence
+      nodes = edges.flatMap(y => List( (y._1, y._1) , (y._2, y._2))).toSet.toList
+      edgeRdd = sc.parallelize(edges.map(e => Edge(e._1, e._2, e._3)))
+      vertexRDD = sc.parallelize(nodes)
+      graph = Graph(vertexRDD, edgeRdd)
+    } yield()
+
   }
 }
 
@@ -36,10 +46,13 @@ object App extends zio.App {
   
   def run(args: List[String]) = 
     appLogic.exitCode
-  
-  val appLogic = 
+
+  val conf = new SparkConf().setAppName("").setMaster("")
+  val sc = new SparkContext(conf)
+
+  val appLogic =
     for {
-      _ <- GraphParser.parse("./data.txt")
+      _ <- GraphParser.parse("./data.txt", sc)
       _ <- printLine("Hello World")
     } yield()
 
