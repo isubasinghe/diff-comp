2aa592eca737e293d8d5aa598a6571b2f80d95ac
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-09-27 22:45:39 +1000

    Started adding types for joined data

--- code/dida-rust/src/shared/graph_data.rs
+++ code/dida-rust/src/shared/graph_data.rs
@@ -1,6 +1,5 @@
-use std::hash::Hash;
 use abomonation_derive::Abomonation;
-
+use std::hash::Hash;
 
 pub trait Unbind<T> {
     fn unbind(&self) -> T;
@@ -10,64 +9,61 @@ pub trait Bind<T> {
     fn bind(&self) -> T;
 }
 
-
 #[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Copy, Clone, Abomonation)]
 pub struct Node<G> {
-    pub id: G
+    pub id: G,
 }
 
 impl<G> Node<G>
 where
-    G: Ord + Copy + Clone
+    G: Ord + Copy + Clone,
 {
-    fn to_outgoing_edge(&self) ->  FromEdge<G> {
+    fn to_outgoing_edge(&self) -> FromEdge<G> {
         self.unbind().bind()
     }
 }
 
 impl<G> Bind<Node<G>> for G
 where
-    G: Copy + Clone
+    G: Copy + Clone,
 {
     fn bind(&self) -> Node<G> {
-        Node{id: *self}
+        Node { id: *self }
     }
 }
 
 impl<G> Unbind<G> for Node<G>
 where
-    G: Copy + Clone
+    G: Copy + Clone,
 {
     fn unbind(&self) -> G {
         (*self).id
     }
 }
 
-
 #[derive(Debug, Default, Hash, Copy, Clone, Abomonation)]
 pub struct FromEdge<G>
 where
-    G: PartialEq + Eq + PartialOrd + Ord
+    G: PartialEq + Eq + PartialOrd + Ord,
 {
-    pub from: G
+    pub from: G,
 }
 
 #[derive(Debug, Default, Hash, Copy, Clone, Abomonation)]
 pub struct ToEdge<G>
 where
-    G: PartialEq + Eq + PartialOrd + Ord
+    G: PartialEq + Eq + PartialOrd + Ord,
 {
     pub to: G,
-    pub weight: u32
+    pub weight: u32,
 }
-
 
 pub type Edge<G> = (FromEdge<G>, ToEdge<G>);
 
 // Impls for ToEdge
 impl<G> PartialEq for ToEdge<G>
 where
-    G: PartialEq + Eq + PartialOrd + Ord
+    G: PartialEq + Eq + PartialOrd + Ord,
 {
     fn eq(&self, other: &Self) -> bool {
         self.to.eq(&other.to)
@@ -76,22 +72,18 @@ where
 
 impl<G> PartialEq<Node<G>> for ToEdge<G>
 where
-    G: PartialEq + Eq + PartialOrd + Ord
+    G: PartialEq + Eq + PartialOrd + Ord,
 {
     fn eq(&self, other: &Node<G>) -> bool {
         self.to.eq(&other.id)
     }
 }
 
-impl<G> Eq for ToEdge<G>
-where
-    G: PartialEq + Eq + PartialOrd + Ord
-{}
-
+impl<G> Eq for ToEdge<G> where G: PartialEq + Eq + PartialOrd + Ord {}
 
 impl<G> PartialOrd for ToEdge<G>
 where
-    G: PartialEq + Eq + PartialOrd + Ord
+    G: PartialEq + Eq + PartialOrd + Ord,
 {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         self.to.partial_cmp(&other.to)
@@ -100,16 +92,16 @@ where
 
 impl<G> PartialOrd<Node<G>> for ToEdge<G>
 where
-    G: PartialEq + Eq + PartialOrd + Ord
+    G: PartialEq + Eq + PartialOrd + Ord,
 {
     fn partial_cmp(&self, other: &Node<G>) -> Option<std::cmp::Ordering> {
-        return self.to.partial_cmp(&other.id)
+        return self.to.partial_cmp(&other.id);
     }
 }
 
 impl<G> Ord for ToEdge<G>
 where
-    G: PartialEq + Eq + PartialOrd + Ord
+    G: PartialEq + Eq + PartialOrd + Ord,
 {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.to.cmp(&other.to)
@@ -117,44 +109,39 @@ where
 }
 // End Impls for ToEdge
 
-
 // Impls for FromEdge
 impl<G> Bind<FromEdge<G>> for G
 where
-    G: Ord + Copy + Clone
+    G: Ord + Copy + Clone,
 {
     fn bind(&self) -> FromEdge<G> {
-        FromEdge{from: *self}
+        FromEdge { from: *self }
     }
 }
 
 impl<G> Unbind<G> for FromEdge<G>
 where
-    G: Ord + Copy + Clone
+    G: Ord + Copy + Clone,
 {
     fn unbind(&self) -> G {
         (*self).from
     }
 }
 
-
 impl<G> PartialEq for FromEdge<G>
 where
-    G: PartialEq + Eq + PartialOrd + Ord
+    G: PartialEq + Eq + PartialOrd + Ord,
 {
     fn eq(&self, other: &Self) -> bool {
         self.from.eq(&other.from)
     }
 }
 
-impl<G> Eq for FromEdge<G>
-where
-    G: PartialEq + Eq + PartialOrd + Ord
-{}
+impl<G> Eq for FromEdge<G> where G: PartialEq + Eq + PartialOrd + Ord {}
 
 impl<G> PartialOrd for FromEdge<G>
 where
-    G: PartialEq + Eq + PartialOrd + Ord
+    G: PartialEq + Eq + PartialOrd + Ord,
 {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         self.from.partial_cmp(&other.from)
@@ -163,7 +150,7 @@ where
 
 impl<G> Ord for FromEdge<G>
 where
-    G: PartialEq + Eq + PartialOrd + Ord
+    G: PartialEq + Eq + PartialOrd + Ord,
 {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.from.cmp(&other.from)
@@ -199,11 +186,25 @@ impl Ord for Community {
 
 impl Community {
     fn new(id: u32) -> Community {
-        Community{id, weights: 1}
+        Community { id, weights: 1 }
     }
 }
 
+#[derive(Debug, Copy, Clone, Hash, Abomonation)]
+pub struct CommEdge<G: Ord + PartialOrd + PartialEq + Eq> {
+    pub comm: Community,
+    pub edge: ToEdge<G>,
+}
 
-fn example<G: Ord + Copy + Clone>(n: Node<G>) -> FromEdge<G> {
+impl<G> CommEdge<G>
+where
+    G: Ord + PartialOrd + PartialOrd + PartialEq + Eq,
+{
+    pub fn new(c: Community, e: ToEdge<G>) -> CommEdge<G> {
+        CommEdge { comm: c, edge: e }
+    }
+}
+
+fn _example<G: Ord + Copy + Clone>(n: Node<G>) -> FromEdge<G> {
     n.unbind().bind()
 }
